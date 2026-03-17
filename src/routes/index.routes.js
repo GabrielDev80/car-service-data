2eb392fe6a207bcb9e9882228fe538a7979f7abb
Gabriel Nicolosi <gabianp@live.com.ar>
2026-03-17 20:51:16 -0300

    Index routes updated

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 import userRouter from "./user.routes.js";
 import sessionRouter from "./session.routes.js";
 import vehicleRouter from "./vehicle.routes.js";
-import { sessionInitVeryfication } from "../middlewares/common.middlewares.js";
+import { jwtAuth } from "../middlewares/jwt.middleware.js";
 
 const indexRouter = Router();
 
+// Routes that handle authentication (register / login / logout)
 indexRouter.use("/v1/api/sessions", sessionRouter);
-indexRouter.use("/v1/api/users", sessionInitVeryfication, userRouter);
-indexRouter.use("/v1/api/vehicles", sessionInitVeryfication, vehicleRouter);
+
+// Protected routes: require an access token
+indexRouter.use("/v1/api/users", jwtAuth, userRouter);
+indexRouter.use("/v1/api/vehicles", jwtAuth, vehicleRouter);
 
 export default indexRouter;
